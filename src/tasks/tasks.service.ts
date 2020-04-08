ac56db8be2318da21b109c3c9ef4895860f83969
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-08 00:16:13 -0300

    :sparkles: Adding HTTP Exceptions (NotFound)

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { TaskRequest } from './dto/task-request';
@@ -34,7 +34,13 @@ export class TasksService {
   }
 
   byId(id: string): Task {
-    return this.tasks.find(task => task.id === id);
+    const found = this.tasks.find(task => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException()
+    }
+
+    return found;
   }
 
   create(taskRequest: TaskRequest): Task {
