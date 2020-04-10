98dee3c55080d899154fe4c1df39fe7ee1c1f762
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-09 22:47:59 -0300

    :wrench: Setting up TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmCoreModule.forRoot(typeORMConfig),
+    TasksModule,
+  ],
 })
-export class AppModule {}
+export class AppModule {
+}
