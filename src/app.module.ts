6a5f67de59180045b08ecfb3d9198886bab56c9f
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-13 23:51:19 -0300

    :sparkles: Adding basic generated files for Auth moudle

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmCoreModule } from '@nestjs/typeorm/dist/typeorm-core.module';
 import { typeORMConfig } from './config/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmCoreModule.forRoot(typeORMConfig),
     TasksModule,
+    AuthModule,
   ],
 })
 export class AppModule {
