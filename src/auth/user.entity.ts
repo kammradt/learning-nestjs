6fd5280564716bdca44f817e465282b4f04c40a3
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-15 19:28:21 -0300

    🗃 Changing entity User to Users

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,6 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity()
+@Entity('users')
 export class User extends BaseEntity {
 
   @PrimaryGeneratedColumn('uuid')
