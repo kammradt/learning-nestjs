57a043a791c9d99bd956d60ae0bc63a208a8bab8
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-25 18:38:20 -0300

    :white_check_mark: Creating tests for UserRepository

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,19 +2,23 @@ import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthRequest } from './dto/auth-request';
 import * as bcrypt from 'bcryptjs';
-import { NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { ConflictException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async persist(authRequest: AuthRequest): Promise<void> {
     const { username, password } = authRequest;
 
-    const user = new User();
+    const user = this.create();
     user.username = username;
     user.salt = await bcrypt.genSalt();
     user.password = await this.hashPassword(password, user.salt);
 
-    await user.save();
+    try {
+      await user.save();
+    } catch (e) {
+      throw new ConflictException("Username should be unique")
+    }
   }
 
   async findByUsername(username: string): Promise<User> {
