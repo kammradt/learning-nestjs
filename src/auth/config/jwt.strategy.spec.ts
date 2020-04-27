b6ee4d36a106d3d149738525cb5b7ebe2d6cdeb8
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-26 21:10:57 -0300

    :white_check_mark: Creating tests for JwtStrategy

---
+++ src/auth/config/jwt.strategy.spec.ts
@@ -0,0 +1,37 @@
+import { JwtStrategy } from './jwt.strategy';
+import { UserRepository } from '../user.repository';
+import { Test } from '@nestjs/testing';
+import { User } from '../user.entity';
+
+const mockUserRepository = () => ({
+  findByUsername: jest.fn(),
+});
+
+describe('JwtStrategy', () => {
+  let jwtStrategy: JwtStrategy;
+  let userRepository;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        JwtStrategy,
+        { provide: UserRepository, useFactory: mockUserRepository },
+      ],
+    }).compile();
+    jwtStrategy = await module.get<JwtStrategy>(JwtStrategy);
+    userRepository = await module.get<UserRepository>(UserRepository);
+  });
+
+  describe('validatePassword', () => {
+    it('should return the User username', async () => {
+      const user = new User();
+      user.username = 'usernameTest'
+      userRepository.findByUsername.mockResolvedValue(user)
+
+      const result = await jwtStrategy.validate({username: 'usernameTest'})
+      expect(userRepository.findByUsername).toHaveBeenCalledWith('usernameTest')
+      expect(result.username).toBe(user.username)
+    });
+    
+  })
+});
