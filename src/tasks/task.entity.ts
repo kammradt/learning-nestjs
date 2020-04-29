808eca21695b3034f85715faa81411511cd153b3
Kammradt <vinicius.kammradt1@gmail.com>
2020-04-29 00:03:14 -0300

    :sparkles: Adding @Exclude to hide sensitive information on response

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,6 +1,7 @@
 import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
 import { User } from '../auth/user.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity()
 export class Task extends BaseEntity {
@@ -17,8 +18,11 @@ export class Task extends BaseEntity {
   @Column()
   status: TaskStatus;
 
+  @Exclude()
   @ManyToOne(type => User, user => user.tasks, { eager: false })
   user: User;
+
+  @Exclude()
   @Column()
   userId: string;
 
